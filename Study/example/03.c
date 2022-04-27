83a367208cc64a3b122366f02323ec2cd820a852
Min Jae Kim <exploit017@gmail.com>
2022-04-27 19:27:13 +0900

    move directory

---
+++ Study/example/03.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void getVal(float *, int); // 가
+
+int main(void) {
+    int i;
+    float score[3] = {0};
+    getVal(score, 3);
+
+    for (i = 0; i < 3; i++) {
+        printf("score[%d]: %f\n", i, score[i]);
+    }
+
+    return 0;
+}
+
+void getVal(float *a, int b) { // 나
+    int i;
+    for (i = 0; i < b; i++) // 다
+    {
+        printf("a[%d]: \n", i);
+        scanf("%f", a + i); // 라
+    }
+}
