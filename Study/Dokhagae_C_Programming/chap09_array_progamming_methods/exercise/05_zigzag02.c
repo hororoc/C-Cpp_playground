940827596f531792329d385374c2f1247299ca7b
Min Jae Kim <exploit017@gmail.com>
2022-05-14 15:47:51 +0900

    배열 프로그래밍 방법 done

--- Study/Dokhagae_C_Programming/chap09_array_progamming_methods/exercise/05_zigzag02.c
+++ Study/Dokhagae_C_Programming/chap09_array_progamming_methods/exercise/05_zigzag02.c
@@ -5,7 +5,7 @@ int main(void) {
     int i = 0, j = 0, nCounter = 0, nOffset = 1;
 
     for (i = 0; i < 5; i++) {
-        // 홀수 행과 짝수 행을 구별하고 첫 번째 요소의 초깃값을 결정함.
+        // 첫 번째 요소의 초깃값을 결정
         if (i % 2 == 0) {
             nCounter = i * 5;
         } else {
@@ -18,8 +18,8 @@ int main(void) {
             nCounter += nOffset;
             aList[i][j] = nCounter;
         }
-
-        //
+        // 토글 스위치처럼 행마다 양수/음수로 변경됨.
+        nOffset = -nOffset;
     }
 
     for (i = 0; i < 5; i++) {
