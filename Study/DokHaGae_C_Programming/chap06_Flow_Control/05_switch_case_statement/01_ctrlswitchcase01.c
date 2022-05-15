618ee50d44261e886c4e493d3afa8167a20fa196
Min Jae Kim <exploit017@gmail.com>
2022-05-15 18:59:51 +0900

    seperate ui & feature

--- Study/DokHaGae_C_Programming/chap06_Flow_Control/05_switch_case_statement/01_ctrlswitchcase01.c
+++ Study/DokHaGae_C_Programming/chap06_Flow_Control/05_switch_case_statement/01_ctrlswitchcase01.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     char cOperator = 0;
     int x = 0, y = 0, nResult = 0;
 
@@ -8,21 +9,22 @@ int main(void) {
     scanf("%d%c%d", &x, &cOperator, &y);
 
     // 연산자가 무엇인지 선택.
-    switch (cOperator) {
-    case '+':
-        nResult = x + y;
-        break;
-    case '-':
-        nResult = x - y;
-        break;
-    case '*':
-        nResult = x * y;
-        break;
-    case '/':
-        nResult = x / y;
-        break;
-    default:
-        puts("ERROR: 알 수 없는 산술 연산입니다.");
+    switch (cOperator)
+    {
+        case '+':
+            nResult = x + y;
+            break;
+        case '-':
+            nResult = x - y;
+            break;
+        case '*':
+            nResult = x * y;
+            break;
+        case '/':
+            nResult = x / y;
+            break;
+        default:
+            puts("ERROR: 알 수 없는 산술 연산입니다.");
     }
 
     printf("Result: %d\n", nResult);
