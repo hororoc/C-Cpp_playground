ee342d24943410b8164325d8ea505186b12fec38
Min Jae Kim <exploit017@gmail.com>
2022-07-05 01:24:51 +0900

    linear-search / binary-search

---
+++ 1_Data-Structure/01_Linear/01_Array/01_remove.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void deleteData(int* ap, int* pcount, int index);
+void insertData(int* ap, int* pcount, int index, int data);
+void printArray(int* ap, int cnt);
+
+int main()
+{
+    int arr_num[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int count = 10;
+}
